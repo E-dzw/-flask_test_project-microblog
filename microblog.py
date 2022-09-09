cbeea0436b2c9d5a5fb7539aeaf99ddeb475b223
Administrator <admin@example.com>
2022-09-09 11:24:32 +0800

    test

--- microblog.py
+++ microblog.py
@@ -1,1 +1,8 @@
-from app import app
+import imp
+from re import U
+from sqlite3 import dbapi2
+from app import app, db
+from app.models import User, Post
+@app.shell_context_processor
+def make_shell_test():
+    return {'db': db ,'user': User,'post': Post}
