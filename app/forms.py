3fe7373c7a88faf431abda1770efac06781c976f
Administrator <admin@example.com>
2022-09-26 11:41:02 +0800

    ys final

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,6 @@
-from xml.dom import ValidationErr
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField,SubmitField
-from wtforms.validators import DataRequired,Email,EqualTo
+from wtforms import StringField, PasswordField, BooleanField,SubmitField,TextAreaField
+from wtforms.validators import DataRequired,Email,EqualTo,ValidationError,Length
 from app.models import User
 
 
@@ -18,12 +17,29 @@ class RegistrationForm(FlaskForm):
     email=StringField("E-mail",validators=[DataRequired(),Email()])
     submit=SubmitField("Register")
 
-    def validata_username(self,username):
+    def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user is not None:
-            return ValidationErr("%s is registrated, please use another username without registrated !!!"%user.username)
+            raise ValidationError("{} is registrated, please use defferent username.".format(username.data))
 
-    def validata_email(self,email):
+    def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
-            return ValidationErr("%s is registrated, please use another email without registrated !!!"%user.email)
+            raise ValidationError("{} is registrated, please use defferent email".format(user.email))
+
+class EditProfileForm(FlaskForm):
+    username=StringField("Username",validators=[DataRequired()])
+    about_me=TextAreaField("About me",validators=[Length(min=0, max=140)])
+    submit=SubmitField("Submit")
+    
+    #表单类不能覆盖FlaskForm类的init的方法,如果需要重写init方法,那么必须在init方法内显示的执行父类的init方法.
+    def __init__(self, original_username, *args, **kwargs):  
+        # ,formdata=..., **kwargs
+        super().__init__(*args, **kwargs)
+        self.original_username=original_username
+    
+    def validate_username(self,username):
+        if self.original_username != username.data:
+            user=User.query.filter_by(username=username.data).first()
+            if user is not None:
+                raise ValidationError("please use different username.")
